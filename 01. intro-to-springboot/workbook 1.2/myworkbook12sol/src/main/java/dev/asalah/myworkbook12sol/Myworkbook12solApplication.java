6be5b0cfe325f5833ee79c0ca41f82229f95b82c
Ahmed Salah <ahmed.mostafa@segmatek.com>
2022-11-18 23:56:01 +0200

    Solve workbook 1.2

---
+++ 01. intro-to-springboot/workbook 1.2/myworkbook12sol/src/main/java/dev/asalah/myworkbook12sol/Myworkbook12solApplication.java
@@ -0,0 +1,13 @@
+package dev.asalah.myworkbook12sol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myworkbook12solApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myworkbook12solApplication.class, args);
+	}
+
+}
---
+++ 01. intro-to-springboot/workbook 1.2/myworkbook12sol/src/test/java/dev/asalah/myworkbook12sol/Myworkbook12solApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.asalah.myworkbook12sol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myworkbook12solApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
