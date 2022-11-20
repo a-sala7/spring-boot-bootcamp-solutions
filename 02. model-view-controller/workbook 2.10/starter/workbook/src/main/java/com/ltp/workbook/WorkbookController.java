937add1a9d5fd8ca280f11827659f8c2237abd80
Ahmed Salah <ahmed.mostafa@segmatek.com>
2022-11-20 11:30:25 +0200

    Solve workbook 2.10

--- 02. model-view-controller/workbook 2.10/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 02. model-view-controller/workbook 2.10/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -11,9 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WorkbookController {
 
+    List<Employee> staff = Arrays.asList(
+        new Employee("Jim Halpert", 32, "Salesman"),
+        new Employee("Andy Bernard", 38, "Salesman"),
+        new Employee("Pam Beesly", 32, "Receptionist"),
+        new Employee("Michael Scott", 49, "Regional Manager"),
+        new Employee("Ryan Howard", 28, "Temp"),
+        new Employee("Angela Martin", 35, "Accountant"),
+        new Employee("Dwight Schrute", 37, "Assistant to the Regional Manager"),
+        new Employee("Gabe Lewis", 38, "Resident Skeleton")
+    );
     
     @GetMapping(value="/")
     public String getStaff(Model model) {
+        model.addAttribute("staff", staff);
         return "staff";
     }
     
